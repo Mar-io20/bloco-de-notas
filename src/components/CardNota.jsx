5b5799ae6b384c4680593a3e52f1ed4642589f55
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-11-26 00:29:23 -0300

    Criando um card para as notas

---
+++ src/components/CardNota.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class CardNota extends Component {
+  render() {
+    return (
+      <section>
+        <h3>Titulo</h3>
+        <p>Escreva sua nota</p>
+      </section>
+    );
+  }
+}
+
+export default CardNota;
