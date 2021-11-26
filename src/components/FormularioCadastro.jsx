ab9217b751e7584d7b85a26050b0dda6b82849e6
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-11-25 23:57:55 -0300

    Crinando compontents

---
+++ src/components/FormularioCadastro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+
+class FormularioCadastro extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="titulo" />
+        <textarea placeholder="Escreva sua nota..." />
+        <button>Criar nota</button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
